468e1725f888e21ab045cbfb8f7a56bcbfe89cf6
biloholovskyi <51454763+biloholovskyi@users.noreply.github.com>
2022-09-03 19:03:16 +0300

    testing router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Outlet } from 'react-router-dom'
 import Home from './routers/home/home'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h2>I am a navigation bar block bitch</h2>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return <div>I am shop page</div>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path={'/'} element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   )
 }
