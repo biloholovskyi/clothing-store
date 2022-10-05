b81276c55269a2f6c85e920b92eb5a799e4149bd
biloholovskyi <51454763+biloholovskyi@users.noreply.github.com>
2022-10-05 14:35:13 +0300

    project complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch } from 'react-redux';
 
-import Home from './routers/home/home'
-import Navigation from './routers/navigation/navigation.component'
-import Authentication from './routers/authentication/authentication.component'
-import Shop from './routers/shop/shop.component'
-import Checkout from './routers/checkout/checkout.component'
+import { Routes, Route } from 'react-router-dom';
+
+import Spinner from './components/spinner/spinner.component';
+import { checkUserSession } from './store/user/user.action';
+import { GlobalStyle } from './global.styles';
+
+const Navigation = lazy(() =>
+  import('./routes/navigation/navigation.component')
+);
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
+const Authentication = lazy(() =>
+  import('./routes/authentication/authentication.component')
+);
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
-  )
-}
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, []);
+
+  return (
+    <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='shop/*' element={<Shop />} />
+          <Route path='auth' element={<Authentication />} />
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
-import { UserProvider } from './contexts/user.context'
-import { CategoriesProvider } from './contexts/categories.context'
-import { CartProvider } from './contexts/cart.context'
+import App from './App';
+import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+import * as registerServiceWorker from './serviceWorkerRegistration';
 
-import App from './App'
+const rootElement = document.getElementById('root');
 
-import './index.scss'
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
             <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  rootElement
+);
+
+registerServiceWorker.register();
